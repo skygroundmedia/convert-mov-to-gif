8c4b2db62904321b6d7a4d749f0eccbfdf9aadfc
Chris Mendez <mail@chrisaiv.com>
2017-02-01 13:03:42 -0800

    Added a few more examples of frame dimensions

--- Converter.rb
+++ Converter.rb
@@ -6,17 +6,20 @@ class Converter
   # Default path to gifsicle installed by homebrew
   @@gifsicle = File.join('/', 'usr', 'local', 'bin', 'gifsicle')
   # Tell ffmpeg the max-width and max-height
-  @@max_size = "1280x1024"
+  @@max_size = ["1280x1024", "800x640", "640x512", "600x480", "480x384", "300x240"][2]
+  @@pixel_format = "rgb8"
   # Tell ffmpeg to reduce the frame rate from to 30
-  @@frames = "30"
+  @@frames = 30
+
   # Tell gifsicle to delay 30ms between each gif (in ms)
   @@delay = 3
   # Requests that gifsicle use the slowest/most file-size optimization
   @@optimize = 3
   
   def initialize(input, output)
-    shell_command = %{#{@@ffmpeg} -i #{input} -s #{@@max_size} -pix_fmt rgb24 -r #{@@frames} -f gif - | #{@@gifsicle} --optimize=#{@@optimize} --delay=#{@@delay} > #{output}}
+    shell_command = %{#{@@ffmpeg} -i #{input} -s #{@@max_size} -pix_fmt #{@@pixel_format} -r #{@@frames} -f gif - | #{@@gifsicle} --optimize=#{@@optimize} --delay=#{@@delay} > #{output}}
     # puts command
+    
     %x[ #{shell_command} ]
   end  
 end
